d0fad050307e8e86be9ff3aad2c53197287f5364
Tanmay Chaturvedi <tanmayreadsandwrites@gmail.com>
2025-02-26 19:17:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+const port = 6532
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/contact', (req, res) => {
+    res.send('unreachable lol')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1>you dare login here</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`)
+})
